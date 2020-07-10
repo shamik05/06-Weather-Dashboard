602c45c6ba33882c5b2db2fb20f7c38971d4dc63
Shamik H <shamik05@hotmail.com>
2020-07-10 18:52:04 -0400

    Added eventlistener to call ajax function

--- Assets/script.js
+++ Assets/script.js
@@ -1,45 +1,20 @@
-$(document).ready(function(){
-    $("#searchBtn").on("click",function(){
-        $("result").text("test");
-        console.log("test");
-    })
-   
-    // function loadJSON(callback) {   
+let searchBtn = document.getElementById("searchBtn")
+let searchText = document.getElementById("searchText")
 
-    //     var xobj = new XMLHttpRequest();
-    //         xobj.overrideMimeType("application/json");
-    //     xobj.open('GET', '../../../city.list.json', true); // Replace 'my_data' with the path to your file
-    //     xobj.onreadystatechange = function () {
-    //           if (xobj.readyState == 4 && xobj.status == "200") {
-    //             // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-    //             callback(xobj.responseText);
-    //           }
-    //     };
-    //     xobj.send(null);  
-    //  }
 
-    //  function init() {
-    //     loadJSON(function(response) {
-    //      // Parse JSON string into object
-    //        var actual_JSON = JSON.parse(response);
-    //        console.log(actual_JSON);
-    //     });
-    //     }
+weatherAjax = ()=>{
+const key = "73dabd1a8d2803884f5955e9d62f1b8e";
+const queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + searchBtn.value + "&appid=" + key;
 
-    // init();
-
-    // var city = "Toronto"
-    // var key = "73dabd1a8d2803884f5955e9d62f1b8e";
-    // var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key;
-    
-    // $.ajax({
-    //     url: queryURL,
-    //     method: "GET"
-    // }).then(function(response){
-    //     console.log(response);
-    //     $(".result").html("<h1>" + response.name + Date.now)
-    //     $(".result").t
-
-    // })
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response){
+    console.log(response);
+    $(".result").html("<h1>" + response.name + Date.now)
+    $(".result").t
 
 })
+}
+
+searchBtn.addEventListener("click", weatherAjax)
