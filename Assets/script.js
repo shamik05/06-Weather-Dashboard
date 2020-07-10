f752dc34b4358712601c3a69809ce776909c0bfd
Shamik H <shamik05@hotmail.com>
2020-07-10 19:32:02 -0400

    Added event to add search to list

--- Assets/script.js
+++ Assets/script.js
@@ -1,10 +1,11 @@
 let searchBtn = document.getElementById("searchBtn")
 let searchText = document.getElementById("searchText")
-
+let searchList = document.getElementById("searchList")
+// console.log();
 
 weatherAjax = ()=>{
 const key = "73dabd1a8d2803884f5955e9d62f1b8e";
-const queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + searchBtn.value + "&appid=" + key;
+const queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + searchText.value + "&appid=" + key;
 
 $.ajax({
     url: queryURL,
@@ -17,4 +18,11 @@ $.ajax({
 })
 }
 
-searchBtn.addEventListener("click", weatherAjax)
+// searchBtn.addEventListener("click", weatherAjax)
+searchBtn.addEventListener("click", function(){
+    let list = document.createElement("li");
+    let listText = document.createTextNode(searchText.value)
+    list.appendChild(listText);
+    list.classList.add("list-group-item")
+    searchList.appendChild(list);
+})
